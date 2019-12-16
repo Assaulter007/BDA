bbfcd25b1cf34c827460dc65494b932b49f79000
Assaulter007 <43980745+Assaulter007@users.noreply.github.com>
2019-12-16 10:36:38 +0530

    Update p14.scala

--- p14.scala
+++ p14.scala
@@ -39,3 +39,17 @@ object weather {
 
   }
 }
+
+
+################################## p14.sbt #######################################
+
+name := "weather"
+ 
+version := "1.0"
+scalaVersion := "2.11.12"
+
+libraryDependencies ++= Seq(
+    "org.apache.spark" %% "spark-core" % "2.3.0",
+    "org.apache.spark" %% "spark-streaming" % "2.3.0",
+    "org.apache.spark" %% "spark-sql" % "2.3.0"
+)
