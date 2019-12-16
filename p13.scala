73b8f3d6ebb4156aae776a4060e1b4797883bc22
Assaulter007 <43980745+Assaulter007@users.noreply.github.com>
2019-12-16 08:38:48 +0530

    Update p13.scala

--- p13.scala
+++ p13.scala
@@ -43,3 +43,16 @@ object ipl {
     println("Percentage of times Toss Winners have won the match = " + (M*100.0)/N + "%")
   }
 }
+
+#############################  ipl.sbt  ##############################################
+
+name := "weather"
+ 
+version := "1.0"
+scalaVersion := "2.11.12"
+
+libraryDependencies += Seq(
+    "org.apache.spark" %% "spark-core" % "2.3.0",
+    "org.apache.spark" %% "spark-streaming" % "2.3.0",
+    "org.apache.spark" %% "spark-sql" % "2.3.0"
+)
