c5ba7c64e38ff89bd725b569748362d3056ddba2
Assaulter007 <43980745+Assaulter007@users.noreply.github.com>
2019-12-20 18:36:35 +0530

    Update p11.scala

--- p11.scala
+++ p11.scala
@@ -27,3 +27,9 @@ object wordcount {
     
   }
 }
+
+#################################wordCount.sbt###############################
+name := "wordCount"
+version := "1.0"
+scalaVersion := "2.11.12"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
